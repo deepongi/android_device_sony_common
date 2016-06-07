0832b638d9e1670fac3aecc507ccf3803f4056dc
Zito <zzitoz@gmail.com>
2016-06-07 23:46:36 +0100

    add aosp recovery/sony init
    Change-Id: I150f9c788592da4831b8c5b1b35fc2815b77afb7

--- common.mk
+++ common.mk
@@ -12,6 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Init
+PRODUCT_PACKAGES += \
+    init_sony \
+    keycheck \
+    toybox_static
+
 DEVICE_PACKAGE_OVERLAYS += device/sony/common/overlay
 
 # Common Specific Permissions
